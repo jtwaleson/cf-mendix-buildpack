55e0ae2e6f1c9d0e6b936634fd03a6fbf9a39393
Jouke Waleson <jouke.waleson@mendix.com>
2015-01-18 22:50:55 +0100

    rewrite compile step in python, refactor, cleanup, add newrelic support

--- start.py
+++ start.py
@@ -4,228 +4,239 @@ import os
 import re
 import signal
 import subprocess
-import sys
 import time
+import sys
+sys.path.insert(0, 'lib')
+import buildpackutil
 from m2ee import M2EE, logger
 
-print "Started Mendix Cloud Foundry Buildpack"
+logger.setLevel(10)
 
-logger.setLevel(20)
-
-subprocess.check_call([
-    'sed',
-    '-i',
-    's|BUILD_PATH|%s|g; s|9000|%d|'
-    % (os.getcwd(), int(os.environ.get('PORT')) + 1),
-    '.local/m2ee.yaml'
-])
-
-if os.environ.get('VCAP_APPLICATION'):
-    vcap_app = json.loads(os.environ.get('VCAP_APPLICATION'))
-else:
-    vcap_app = {
-        'application_uris': ['example.com'],
-        'application_name': 'My App',
-    }
+logger.info('Started Mendix Cloud Foundry Buildpack')
 
 
-def get_database_config(url):
-    pattern = r'([a-zA-Z]+)://([^:]+):([^@]+)@([^/]+)/(.*)'
-    match = re.search(pattern, url)
-    type_map = {
-        'postgres':  'PostgreSQL',
-        'mysql': 'MySQL',
-    }
+def pre_process_m2ee_yaml():
+    runtime_port = int(os.environ['PORT'])
 
-    if match is None:
-        raise Exception(
-            "Could not parse DATABASE_URL environment variable %s" % url
-        )
+    subprocess.check_call([
+        'sed',
+        '-i',
+        's|BUILD_PATH|%s|g; s|RUNTIME_PORT|%d|; s|ADMIN_PORT|%d|'
+        % (os.getcwd(), runtime_port, runtime_port + 1),
+        '.local/m2ee.yaml'
+    ])
 
-    database_type_input = match.group(1)
-    if match.group(1) not in type_map:
-        raise Exception("Unknown database type: %s", database_type_input)
-    database_type = type_map[database_type_input]
 
-    return {
-        'DatabaseType': database_type,
-        'DatabaseUserName': match.group(2),
-        'DatabasePassword': match.group(3),
-        'DatabaseHost': match.group(4),
-        'DatabaseName': match.group(5),
-    }
+def get_vcap_data():
+    if os.environ.get('VCAP_APPLICATION'):
+        return json.loads(os.environ.get('VCAP_APPLICATION'))
+    else:
+        return {
+            'application_uris': ['example.com'],
+            'application_name': 'My App',
+        }
 
-prefs_template = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+
+def activate_license():
+    prefs_template = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE map SYSTEM "http://java.sun.com/dtd/preferences.dtd">
 <map MAP_XML_VERSION="1.0">
   <entry key="id" value="{{SERVER_ID}}"/>
   <entry key="license_key" value="{{LICENSE_KEY}}"/>
 </map>"""
 
-license = os.environ.get('LICENSE_KEY', None)
-server_id = os.environ.get('SERVER_ID', None)
-if license is not None and server_id is not None:
-    print('A license was supplied so going to activate it')
-    prefs_body = prefs_template.replace(
-        '{{SERVER_ID}}', server_id
-        ).replace(
-        '{{LICENSE_KEY}}', license
+    license = os.environ.get('LICENSE_KEY', None)
+    server_id = os.environ.get('SERVER_ID', None)
+    if license is not None and server_id is not None:
+        logger.debug('A license was supplied so going to activate it')
+        prefs_body = prefs_template.replace(
+            '{{SERVER_ID}}', server_id
+            ).replace(
+            '{{LICENSE_KEY}}', license
+            )
+        prefs_dir = os.path.expanduser('~/../.java/.userPrefs/com/mendix/core')
+        if not os.path.isdir(prefs_dir):
+            os.makedirs(prefs_dir)
+        with open(os.path.join(prefs_dir, 'prefs.xml'), 'w') as prefs_file:
+            prefs_file.write(prefs_body)
+
+
+def get_constants(metadata):
+    constants = {}
+    for constant in metadata['Constants']:
+        env = 'MX_%s' % constant['Name'].replace('.', '_')
+        value = os.environ.get(env)
+        if value is None:
+            value = constant['DefaultValue']
+            logger.debug(
+                'constant not found in environment, taking default '
+                'value %s' % constant['Name']
+            )
+        if constant['Type'] == 'Integer':
+            value = int(value)
+        constants[constant['Name']] = value
+    return constants
+
+
+def set_heap_size(javaopts):
+    max_memory = os.environ.get('MEMORY_LIMIT', '512m').upper()
+    match = re.search('([0-9]+)([A-Z])', max_memory)
+    max_memory = '%d%s' % (int(match.group(1)) / 2, match.group(2))
+    heap_size = os.environ.get('HEAP_SIZE', max_memory)
+    javaopts.append('-Xmx%s' % heap_size)
+    javaopts.append('-Xms%s' % heap_size)
+    logger.info('Java heap size set to %s' % max_memory)
+
+
+def set_runtime_config(metadata, mxruntime_config, vcap_data):
+    database_config = buildpackutil.get_database_config()
+    application_config = {
+        'ApplicationRootUrl': 'https://%s' % vcap_data['application_uris'][0],
+        'MicroflowConstants': get_constants(metadata),
+    }
+    runtime_config = dict(database_config.items() + application_config.items())
+
+    for key, value in runtime_config.iteritems():
+        mxruntime_config[key] = value
+
+
+def set_application_name(m2ee, name):
+    logger.debug('Application name is %s' % name)
+    m2ee.config._conf['m2ee']['app_name'] = name
+
+
+def activate_new_relic(m2ee, app_name):
+    if os.environ.get('NEW_RELIC_LICENSE_KEY') is None:
+        logger.debug(
+            'Skipping New Relic setup, no license key found in environment'
         )
-    prefs_dir = os.path.expanduser('~/../.java/.userPrefs/com/mendix/core')
-    if not os.path.isdir(prefs_dir):
-        os.makedirs(prefs_dir)
-    with open(os.path.join(prefs_dir, 'prefs.xml'), 'w') as prefs_file:
-        prefs_file.write(prefs_body)
+        return
+    logger.info('Adding new relic')
+    m2ee_section = m2ee.config._conf['m2ee']
+    if 'custom_environment' not in m2ee_section:
+        m2ee_section['custom_environment'] = {}
+    m2ee_section['custom_environment']['NEW_RELIC_APP_NAME'] = app_name
+    m2ee_section['custom_environment']['NEW_RELIC_LOG'] = (
+        os.path.abspath('newrelic/agent.log')
+    )
 
-m2ee = M2EE(yamlfiles=['.local/m2ee.yaml'], load_default_files=False)
-
-print "Loaded m2ee config file"
-
-
-def sigterm_handler():
-    print('stopping the process')
-    m2ee.stop()
-    print('process stopped')
-    sys.exit(0)
-
-signal.signal(signal.SIGTERM, sigterm_handler)
-
-metadata = json.loads(open('model/metadata.json').read())
-
-print "Metadata successfully read"
-
-constants = {}
-
-for constant in metadata['Constants']:
-    env = 'MX_%s' % constant['Name'].replace('.', '_')
-    value = os.environ.get(env)
-    if value is None:
-        value = constant['DefaultValue']
-        print(
-            'constant not found in environment, taking default '
-            'value %s' % constant['Name']
+    m2ee.config._conf['m2ee']['javaopts'].append(
+        '-javaagent:{path}'.format(
+            path=os.path.abspath('newrelic/newrelic.jar')
         )
-    if constant['Type'] == 'Integer':
-        value = int(value)
-    constants[constant['Name']] = value
-
-db_url = os.environ.get('DATABASE_URL')
-database_config = get_database_config(db_url)
-application_config = {
-    'ApplicationRootUrl': 'https://%s' % vcap_app['application_uris'][0],
-    'MicroflowConstants': constants,
-}
-
-runtime_config = dict(database_config.items() + application_config.items())
-
-for key, value in runtime_config.iteritems():
-    m2ee.config._conf['mxruntime'][key] = value
-
-m2ee.config._conf['m2ee']['runtime_port'] = int(os.environ.get('PORT'))
-m2ee.config._conf['m2ee']['app_name'] = vcap_app['application_name']
-
-print "Application name is %s" % m2ee.config._conf['m2ee']['app_name']
-
-max_memory = os.environ.get('MEMORY_LIMIT', '512m').upper()
-
-match = re.search('([0-9]+)([A-Z])', max_memory)
-max_memory = '%d%s' % (int(match.group(1)) / 2, match.group(2))
-
-heap_size = os.environ.get('HEAP_SIZE', max_memory)
-
-m2ee.config._conf['m2ee']['javaopts'].append('-Xmx%s' % heap_size)
-m2ee.config._conf['m2ee']['javaopts'].append('-Xms%s' % heap_size)
-
-print('Java heap size set to %s' % max_memory)
-
-fs_env = 'STACKATO_FILESYSTEM_%s_FS' % os.environ.get('STACKATO_APP_NAME_UPCASE', None)
-
-application_file_directory = os.path.join('/', 'app', 'data', 'files')
-persistent_file_directory = os.environ.get(fs_env)
-
-if persistent_file_directory is not None:
-    if os.path.isdir(persistent_file_directory):
-        if os.path.isdir(application_file_directory):
-            os.rmdir(application_file_directory)
-        os.symlink(persistent_file_directory, application_file_directory)
-        print "persistent files?: Yes."
-    else:
-        print "Uploaded files will be removed when the application is restarted"
-else:
-    print "persistent files? No."
+    )
 
 
-m2ee.start_appcontainer()
-if not m2ee.send_runtime_config():
-    sys.exit(1)
+def set_up_m2ee_client(vcap_data):
+    m2ee = M2EE(yamlfiles=['.local/m2ee.yaml'], load_default_files=False)
+    set_runtime_config(
+        m2ee.config._model_metadata,
+        m2ee.config._conf['mxruntime'],
+        vcap_data,
+    )
+    set_heap_size(m2ee.config._conf['m2ee']['javaopts'])
+    activate_new_relic(m2ee, vcap_data['application_name'])
+    set_application_name(m2ee, vcap_data['application_name'])
+    return m2ee
 
-print "Appcontainer has been started"
 
-with open('log/out.log', 'a'):
-    os.utime('log/out.log', None)
-subprocess.Popen(['tail', '-f', 'log/out.log'])
+def set_up_logging_file():
+    os.mkfifo('log/out.log')
+    subprocess.Popen(['cat', 'log/out.log'])
 
-abort = False
-success = False
-while not (success or abort):
-    startresponse = m2ee.client.start({'autocreatedb': True})
-    result = startresponse.get_result()
-    if result == 0:
-        success = True
-        print("The MxRuntime is fully started now.")
-    else:
-        startresponse.display_error()
-        if result == 2:
-            print("DB does not exists")
-            abort = True
-        elif result == 3:
-            m2eeresponse = m2ee.client.execute_ddl_commands()
-            m2eeresponse.display_error()
-        elif result == 4:
-            print("Not enough constants!")
-            abort = True
-        elif result == 5:
-            print("Unsafe password!")
-            abort = True
-        elif result == 6:
-            print("Invalid state!")
-            abort = True
-        elif result == 7 or result == 8 or result == 9:
-            print("You'll have to fix the configuration and run start "
-                  "again... (or ask for help..)")
-            abort = True
+
+def start_app(m2ee):
+    m2ee.start_appcontainer()
+    if not m2ee.send_runtime_config():
+        sys.exit(1)
+
+    logger.debug('Appcontainer has been started')
+
+    abort = False
+    success = False
+    while not (success or abort):
+        startresponse = m2ee.client.start({'autocreatedb': True})
+        result = startresponse.get_result()
+        if result == 0:
+            success = True
+            logger.info('The MxRuntime is fully started now.')
         else:
-            abort = True
-if abort:
-    print('start failed, stopping')
+            startresponse.display_error()
+            if result == 2:
+                logger.warning('DB does not exists')
+                abort = True
+            elif result == 3:
+                m2eeresponse = m2ee.client.execute_ddl_commands()
+                m2eeresponse.display_error()
+            elif result == 4:
+                logger.warning('Not enough constants!')
+                abort = True
+            elif result == 5:
+                logger.warning('Unsafe password!')
+                abort = True
+            elif result == 6:
+                logger.warning('Invalid state!')
+                abort = True
+            elif result == 7 or result == 8 or result == 9:
+                logger.warning(
+                    "You'll have to fix the configuration and run start "
+                    "again... (or ask for help..)"
+                )
+                abort = True
+            else:
+                abort = True
+    if abort:
+        logger.warning('start failed, stopping')
+        sys.exit(1)
+
+
+def create_admin_user(m2ee):
+    logger.info('Creating admin user')
+    m2eeresponse = m2ee.client.create_admin_user({
+        'password': os.environ.get('ADMIN_PASSWORD'),
+    })
+    if m2eeresponse.has_error():
+        m2eeresponse.display_error()
+        sys.exit(1)
+
+    logger.info('Setting admin user password')
+    m2eeresponse = m2ee.client.create_admin_user({
+        'username': m2ee.config._model_metadata['AdminUser'],
+        'password': os.environ.get('ADMIN_PASSWORD'),
+    })
+    if m2eeresponse.has_error():
+        m2eeresponse.display_error()
+        sys.exit(1)
+
+
+def display_running_version(m2ee):
+    feedback = m2ee.client.about().get_feedback()
+    logger.info("Using %s version %s" % (feedback['name'], feedback['version']))
+    if m2ee.config.get_runtime_version() >= 4.4:
+        if 'model_version' in feedback:
+            logger.info('Model version: %s' % feedback['model_version'])
+
+
+def loop_until_process_dies(m2ee):
+    while m2ee.runner.check_pid():
+        time.sleep(10)
+    logger.info('process died, stopping')
     sys.exit(1)
 
-print('Creating admin user')
-m2eeresponse = m2ee.client.create_admin_user({
-    'password': os.environ.get('ADMIN_PASSWORD'),
-})
-if m2eeresponse.has_error():
-    m2eeresponse.display_error()
-    sys.exit(1)
 
-print('Setting admin user password')
-m2eeresponse = m2ee.client.create_admin_user({
-    'username': metadata['AdminUser'],
-    'password': os.environ.get('ADMIN_PASSWORD'),
-})
-if m2eeresponse.has_error():
-    m2eeresponse.display_error()
-    sys.exit(1)
+if __name__ == '__main__':
+    pre_process_m2ee_yaml()
+    activate_license()
+    set_up_logging_file()
+    m2ee = set_up_m2ee_client(get_vcap_data())
 
-feedback = m2ee.client.about().get_feedback()
-print("Using %s version %s" % (feedback['name'], feedback['version']))
-if m2ee.config.get_runtime_version() >= 4.4:
-    if 'model_version' in feedback:
-        print('Model version: %s' % feedback['model_version'])
+    def sigterm_handler(_signo, _stack_frame):
+        m2ee.stop()
+        sys.exit(0)
 
-while m2ee.runner.check_pid():
-    print('process still alive, sleeping')
-    time.sleep(10)
+    signal.signal(signal.SIGTERM, sigterm_handler)
 
-print('process died, stopping')
-sys.exit(1)
+    start_app(m2ee)
+    create_admin_user(m2ee)
+    display_running_version(m2ee)
+    loop_until_process_dies(m2ee)
